e64047e0639dafb021d6774170e4e9de8dd2d7ac
Marcos Tenrero <marcos.95tm@gmail.com>
2018-08-20 21:29:06 +0200

    Recovering AppModule hierachy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from 'app.controller';
 import { AppService } from 'app.service';
+import { MessageModule } from './message/message.module';
+import { RethinkModule } from 'rethink/rethink.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MessageModule,
+    RethinkModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { RethinkModule } from 'rethink/rethink.module';
+import { AppModule } from 'app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(RethinkModule);
+  const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 bootstrap();
