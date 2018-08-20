fcc631415a333fdce75ccb146103cc1fcd4a2370
Marcos Tenrero <marcos.95tm@gmail.com>
2018-08-20 21:29:57 +0200

    Message class

---
+++ src/message/message.ts
@@ -0,0 +1,9 @@
+export class Message {
+    author: string
+    message: string
+
+    constructor(author: string, message: string) {
+        this.author = author
+        this.message = message
+    }
+}
